edfdceddfbaabe2867236a490af49b688667ac4c
Lars Trieloff <trieloff@adobe.com>
2019-08-21 15:04:04 +0000

    feat(unfurl): upgrade to unfurl.js 4.0.0
    adjust to breaking changes from upstream package

--- src/embed.js
+++ src/embed.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const unfurl = require('unfurl.js');
+const { unfurl } = require('unfurl.js');
 const URI = require('uri-js');
 const spark = require('./spark');
 const unsplash = require('./unsplash');

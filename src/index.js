b6ad6297d5e4ea8fafe31fa64eefea57c69e686b
Marquise Rosier <mrosier@adobe.com>
2020-11-09 12:04:43 -0800

    fix(src/index.js): add timeout to fastly request (#335)
    * fix(src/index.js): add timeout to fastly request
    
    currently request to fastly public-ip-list times out very frequently
    this leads to action failure as the container has a 6000ms timeout
    on generating an action response. This times out at 3000ms with action
    failure after retries after 500ms timeouts.
    
    fix #326

--- src/index.js
+++ src/index.js
@@ -20,9 +20,8 @@ const {
   embed, getEmbedKind, addTitle, propagateQueryParams,
 } = require('./embed.js');
 const dataSource = require('./data-source.js');
-
-// lazy-loaded public ip list
-let ipList;
+// require ip-list.json which is updated every build
+const ipList = require('../ip-list.json');
 
 /**
  *
@@ -72,13 +71,6 @@ async function serviceembed(params, url, log) {
   const { kind } = params;
   const api = new URL(params.api || params.OEMBED_RESOLVER_URI);
   if (params.OEMBED_RESOLVER_PARAM && params.OEMBED_RESOLVER_KEY) {
-    if (!ipList) {
-      const now = Date.now();
-      // lazy-load public ip list
-      const resp = await fetch('https://api.fastly.com/public-ip-list');
-      ipList = await resp.json();
-      log.info(`fetched public ip list from fastly in ${Date.now() - now}ms`, ipList);
-    }
     if (await isWithinRange(params.__ow_headers['x-forwarded-for'], ipList, params.ALLOWED_IPS)) {
       qs[params.OEMBED_RESOLVER_PARAM] = params.OEMBED_RESOLVER_KEY;
       log.info(`Using embedding service ${api} for URL ${url}`);
